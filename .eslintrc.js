0d5de9ae9de3d9fe4944799f891d32aee6a7c67a
David Afonso <davafons@gmail.com>
2019-07-09 19:59:35 +0100

    :art: Remove CP footer. Change title font.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,4 @@
 module.exports = {
-  globals: {
-    __PATH_PREFIX__: true,
-  },
   env: {
     browser: true,
     es6: true,
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,3 +1,5 @@
+/* global __PATH_PREFIX__ */
+
 import React from "react"
 import { Link } from "gatsby"
 
@@ -34,7 +36,6 @@ class Layout extends React.Component {
       header = (
         <h3
           style={{
-            fontFamily: `Montserrat, sans-serif`,
             marginTop: 0,
           }}
         >
@@ -62,11 +63,6 @@ class Layout extends React.Component {
       >
         <header>{header}</header>
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
       </div>
     )
   }
